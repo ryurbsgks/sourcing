28d27a539bfc12b1927e59ecff5a4e72b99d518d
ryurbsgks <ryurbsgks5114@gmail.com>
2022-09-09 13:29:23 +0900

    Update: Main 페이지 업데이트
    * Main 페이지 업데이트
    - SearchBar 컴포넌트 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,11 @@
 import TopBar from "../component/TopBar";
+import SearchBar from "../component/SearchBar";
 
 function Main() {
   return (
     <header>
       <TopBar />
+      <SearchBar />
     </header>
   );
 }
