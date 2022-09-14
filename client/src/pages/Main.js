7385da13bc2ee96b71f6425a33aebe1b2d7a11f5
ryurbsgks <ryurbsgks5114@gmail.com>
2022-09-14 17:37:42 +0900

    Update: Main 페이지 업데이트
    * Main 페이지 업데이트
    - NavBar 컴포넌트 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,13 @@
 import TopBar from "../component/TopBar";
 import SearchBar from "../component/SearchBar";
+import NavBar from "../component/NavBar";
 
 function Main() {
   return (
     <header>
       <TopBar />
       <SearchBar />
+      <NavBar />
     </header>
   );
 }
