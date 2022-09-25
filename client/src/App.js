c76e6a70799c0a74edbf78c0ca60dbb61762b508
ryurbsgks <ryurbsgks5114@gmail.com>
2022-09-25 19:10:30 +0900

    Feat: Login 페이지 라우팅 추가
    * Login 페이지 라우팅 추가

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
