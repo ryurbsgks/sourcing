eb2acbbd645cdba58a738c01c7fc1f07f4d935c7
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-01 21:18:43 +0900

    Feat: Signup 기능 추가
    * Signup 기능 추가

---
+++ server/controllers/user/signup.js
@@ -0,0 +1,37 @@
+const { user } = require("../../models");
+
+module.exports = (req, res) => {
+
+  const userID = req.body.userID;
+  const pw = req.body.pw;
+  const nickname = req.body.nickname;
+  const tel = req.body.tel;
+  const email = req.body.email;
+  const address = req.body.address;
+
+  if (!userID || !pw || !nickname || !tel) {
+    return res.status(400).send({ message: "잘못된 요청입니다" }); 
+  }
+
+  user.findOrCreate({
+    where: {
+      userID: userID
+    },
+    defaults: {
+      pw: pw,
+      nickname: nickname,
+      tel: tel,
+      email: email,
+      address: address
+    }
+  }).then( ([user, created]) => {
+    if (!created) {
+      return res.status(409).send({ message: "이미 존재하는 아이디입니다" });
+    }
+
+    res.status(201).send({ message: "회원가입 성공" });
+  }).catch( (err) => {
+    res.status(500).send({ message: err });
+  });
+
+};
