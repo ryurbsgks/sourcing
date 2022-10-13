e19a048a8448bad1ca2853075771d13c76abca4e
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-13 18:42:12 +0900

    Refactor: 로그인 JWT 방식 적용
    * 로그인 JWT 방식 적용

--- server/controllers/user/login.js
+++ server/controllers/user/login.js
@@ -1,8 +1,5 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
 const { user } = require("../../models");
-
-dotenv.config();
+const { accessTokenSign, accessTokenSend } = require("../token"); 
 
 module.exports = (req, res) => {
 
@@ -23,19 +20,15 @@ module.exports = (req, res) => {
       return res.status(400).send({ message: "아이디 또는 비밀번호가 일치하지 않습니다" });
     }
 
-    const payload = {
-      id: result.dataValues.id,
-      userID: result.dataValues.userID,
-      nickname: result.dataValues.nickname,
-      tel: result.dataValues.tel,
-      email: result.dataValues.email,
-      address: result.dataValues.address,
-    };
-    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY);
+    delete result.dataValues.pw;
 
-    res.cookie("sourcing", token).status(200).send({ message: "로그인 성공" });
+    const payload = { userInfo: result.dataValues, userStatus: "login" };
+    const accessToken = accessTokenSign(payload);
+
+    accessTokenSend(res, accessToken);
+    res.status(200).send({ message: "로그인 성공" });
   }).catch( (err) => {
     res.status(500).send({ message: err });
-  })
+  });
 
 };
