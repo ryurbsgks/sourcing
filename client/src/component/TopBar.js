2a4e3a9c4fc053a4826d9102063160ee31cf7a1a
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-17 01:40:02 +0900

    Update: 로그인 상태 redux 적용
    * 로그인 상태 redux 적용

--- client/src/component/TopBar.js
+++ client/src/component/TopBar.js
@@ -1,14 +1,24 @@
 import "../App.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function TopBar() {
+
+  const isLogin = useSelector( (state) => state.isLogIn );
+
   return (
     <div className="topbar">
       <div className="container">
-        <ul className="topbar__menu">
-          <li><Link to="/login">로그인</Link></li>
-          <li><Link to="/signup">회원가입</Link></li>
-        </ul>
+        {isLogin 
+        ? <ul className="topbar__menu">
+            <li><Link to="/">마이페이지</Link></li>
+            <li><Link to="/">로그아웃</Link></li>
+          </ul>
+        : <ul className="topbar__menu">
+            <li><Link to="/login">로그인</Link></li>
+            <li><Link to="/signup">회원가입</Link></li>
+          </ul>
+        }
       </div>
     </div>
   );
