bcea62b3e12a8c963251001821f9453700407ab7
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-18 19:17:14 +0900

    Update: Banner 이미지 슬라이드 기능 추가
    * Banner 이미지 슬라이드 기능 추가
    - 이미지 자동 슬라이드
    - 이미지 무한 슬라이드

--- client/src/component/Banner.js
+++ client/src/component/Banner.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
@@ -7,43 +7,86 @@ import { bannerImage } from "../dummydata/dummydata";
 
 function Banner() {
 
-  const [count, setCount] = useState(0);
-  const [slide, setSlide] = useState({
-    width: `${bannerImage.length}00vw`,
-    transform: `translate(-000vw)`
-  })
+  const [count, setCount] = useState(1);
+  const [delay, setDelay] = useState(5000);
+  const slideRef = useRef("transform 0.5s");
+
+  const bannerArr = [bannerImage[bannerImage.length - 1], ...bannerImage, bannerImage[0]];
+  const banner__container = {
+    width: `${bannerArr.length}00vw`,
+    transform: `translate(-${count}00vw)`,
+    transition: `${slideRef.current}`
+  };
 
   useEffect( () => {
-    setSlide({
-      ...slide,
-      transform: `translate(-${count}00vw)`
-    })
+
+    if (count === bannerArr.length - 1) {
+      return setDelay(500);
+    }
+
+    setDelay(5000);
+
   }, [count]);
+
+  const useInterval = (callback, delay) => {
+    
+    const saveCallback = useRef();
+
+    useEffect( () => {
+      saveCallback.current = callback;
+    }, [callback]);
+
+    useEffect( () => {
+
+      const tick = () => {
+        saveCallback.current();
+      };
+
+      if (delay !== null) {
+
+        const id = setInterval(tick, delay);
+
+        return () => clearInterval(id);
+      }
+
+    }, [delay]);
+
+  };
+
+  useInterval( () => {
+    setCount(count + 1);
+  }, delay);
+
+  if (count === bannerArr.length) {
+    if (slideRef.current) {
+      slideRef.current = "";
+    }
+
+    setCount(1);
+
+    setTimeout( () => {
+      if (!slideRef.current) {
+        slideRef.current = "transform 0.5s";
+      }
+    }, 0);
+  }
 
   const handleSlideBtn = (id) => {
 
-    if (id === "left") {
-      if (count === 0) {
-        return setCount(2);
-      }
-
-      setCount(count - 1);
+    if (id === "right") {
+      setCount(count + 1);  
     }
 
-    if (id === "right") {
-      if (count === 2) {
-        return setCount(0);
-      }
-
-      setCount(count + 1);
+    if (id === "left") {
+      setCount(count - 1);
     }
 
   };
 
   return (
     <section className="banner">
-      <div style={slide} className="banner__container">
-        {bannerImage.map( (el, index) => {
+      <div style={banner__container} className="banner__container">
+        {bannerArr.map( (el, index) => {
           return (
             <Link key={index} to="/">
               <img src={require(`../images/${el}`)} alt="BannerImage" />
