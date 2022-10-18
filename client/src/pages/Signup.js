5279035fb1fcb09d9c6f1b91a8b3b7321ad29358
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-18 22:55:30 +0900

    Update: 페이지 접근 권한 설정
    * 페이지 접근 권한 설정

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import TopBar from "../component/TopBar";
 import SearchBar from "../component/SearchBar";
 import NavBar from "../component/NavBar";
 import SignupComponent from "../component/Signup";
+import Check from "../modal/Check";
 
 function Signup() {
+
+  const isLogin = useSelector( (state) => state.isLogIn );
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <header>
@@ -12,6 +23,7 @@ function Signup() {
         <NavBar />
       </header>
       <main>
+        {isLogin ? <Check content={"이미 로그인 상태입니다"} handler={handleNavigate} /> : null}
         <SignupComponent />
       </main>
     </>
