6ae7eec283d7f4c2cca0f891d928d925e4176343
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-22 02:17:10 +0900

    Update: 이메일 인증번호 확인 기능 추가
    * 이메일 인증번호 확인 기능 추가

--- server/controllers/user/verify.js
+++ server/controllers/user/verify.js
@@ -3,12 +3,33 @@ const { verifyToken } = require("../token");
 module.exports = (req, res) => {
 
   const payload = verifyToken(req);
-  const authNumber = String(payload.authNumber);
-  const verifyTel = String(req.body.verifyTel);
 
-  if (verifyTel !== authNumber) {
-    return res.status(403).send({ message: "인증번호가 일치하지 않습니다" });
+  if (!payload) {
+    return res.status(403).send({ message: "인증 유효 시간이 만료되었습니다" });
   }
 
-  res.status(200).send({ message: "인증번호가 일치합니다" });
+  const authNumber = String(payload.authNumber);
+  
+  if (req.body.verifyTel) {
+
+    const verifyTel = String(req.body.verifyTel);
+
+    if (verifyTel !== authNumber) {
+      return res.status(403).send({ message: "인증번호가 일치하지 않습니다" });
+    }
+
+    return res.status(200).send({ message: "인증번호가 일치합니다" });
+  }
+
+  if (req.body.verifyEmail) {
+
+    const verifyEmail = String(req.body.verifyEmail);
+
+    if (verifyEmail !== authNumber) {
+      return res.status(403).send({ message: "인증번호가 일치하지 않습니다" });
+    }
+
+    return res.status(200).send({ message: "인증번호가 일치합니다" });
+  }
+
 };
