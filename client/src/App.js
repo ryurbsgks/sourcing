575c48614b95f9e087eb6d44382e4cf3dd7a83f1
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-24 04:02:57 +0900

    Feat: 아이디, 비밀번호 찾기 UI 추가
    * 아이디, 비밀번호 찾기 UI 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import FindID from "./pages/FindID";
+import FindPW from "./pages/FindPW";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/findid" element={<FindID />} />
+          <Route path="/findpw" element={<FindPW />} />
       </Routes>
     </BrowserRouter>
   );
