3435c7a9c3b58daf32e654530947730685a0f836
ryurbsgks <ryurbsgks5114@gmail.com>
2022-10-25 22:41:16 +0900

    Update: 핸드폰 문자 전송 기능 업데이트
    * 핸드폰 문자 전송 기능 업데이트

--- server/controllers/user/signupSMS.js
+++ server/controllers/user/signupSMS.js
@@ -6,10 +6,11 @@ module.exports = (req, res) => {
 
   const { tel } = req.params;
   const authNumber = authNumberCreate();
+  const content = `[오늘의 장] 인증번호 [${authNumber}]를 입력해주세요`
   const payload = { authNumber: authNumber };
   const authNumberToken = authNumberTokenSign(payload);
 
   authNumberTokenSend(res, authNumberToken, "TelAuthNumber");
-  send(tel, authNumber);
+  send(tel, content);
   res.status(200).send({ message: "인증번호 전송 성공" });
 };
