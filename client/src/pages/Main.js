4de68f1d186da874dc5bc8852c9f715751d0bdea
ryurbsgks <ryurbsgks5114@gmail.com>
2022-11-01 15:44:03 +0900

    Update: 로그인 시 Main 페이지 로그인 상태 처리
    * 로그인 시 Main 페이지 로그인 상태 처리

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,10 +1,28 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import TopBar from "../components/common/TopBar";
 import SearchBar from "../components/common/SearchBar";
 import NavBar from "../components/common/NavBar";
 import Banner from "../components/main/Banner";
 import ProductRecommend from "../components/main/ProductRecommend";
+import { setIsLogin } from "../redux/action";
 
 function Main() {
+
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+
+    if (location.state) {
+      if (location.state.isLogin) {
+        dispatch(setIsLogin(true));
+      }
+    }
+
+  }, []);
+
   return (
     <>
       <TopBar />
