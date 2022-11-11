5fecf1b23f83421eb2e8e8eed1ba6e6e387de713
ryurbsgks <ryurbsgks5114@gmail.com>
2022-11-11 20:52:30 +0900

    Update: 상품 등록 이미지 저장 기능 추가
    * 상품 등록 이미지 저장 기능 추가

--- server/controllers/product/new.js
+++ server/controllers/product/new.js
@@ -1,41 +1,60 @@
 const { product } = require("../../models");
+const multer = require('multer');
 
 module.exports = (req, res) => {
 
-  const category = req.body.category;
-  const pic = req.body.pic;
-  const name = req.body.name;
-  const price = req.body.price;
-  const saleInfo = req.body.saleInfo;
-  const salePrice = req.body.salePrice;
-  const unit = req.body.unit;
-  const weight = req.body.weight;
-  const origin = req.body.origin;
-  const info = req.body.info;
+  let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "img/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${file.originalname}`);
+    }
+  });
+  const upload = multer({ storage: storage }).single("img");
 
-  if (!category || !pic || !name || !price || !unit || !weight || !origin) {
-    return res.status(400).send({ message: "잘못된 요청입니다" }); 
-  }
-
-  product.create({
-    category: category,
-    pic: pic,
-    name: name,
-    price: price,
-    saleInfo: saleInfo,
-    salePrice: salePrice,
-    unit: unit,
-    weight: weight,
-    origin: origin,
-    info: info
-  }).then( (result) => {
-    if (!result) {
-      return res.status(400).send({ message: "상품 등록 실패" });
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(500).send({ message: "업로드 오류" });
     }
 
-    res.status(200).send({ message: "상품 등록 성공" });
-  }).catch( (err) => {
-    res.status(500).send({ message: err });
+    const data = JSON.parse(req.body.data);
+    const category = data.category;
+    const pic = res.req.file.path;
+    const name = data.name;
+    const price = data.price;
+    const saleInfo = data.saleInfo;
+    const salePrice = data.salePrice;
+    const unit = data.unit;
+    const weight = data.weight;
+    const origin = data.origin;
+    const info = data.info;
+
+    if (!category || !pic || !name || !price || !unit || !weight || !origin) {
+      return res.status(400).send({ message: "잘못된 요청입니다" }); 
+    }
+
+    product.create({
+      category: category,
+      pic: pic,
+      name: name,
+      price: price,
+      saleInfo: saleInfo,
+      salePrice: salePrice,
+      unit: unit,
+      weight: weight,
+      origin: origin,
+      info: info
+    }).then( (result) => {
+      if (!result) {
+        return res.status(400).send({ message: "상품 등록 실패" });
+      }
+  
+      return res.status(200).send({ message: "상품 등록 성공" });
+    }).catch( (err) => {
+      res.status(500).send({ message: err });
+    });
+    
   });
 
 };
