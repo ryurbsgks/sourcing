4bf5e1ef4a0d1f8e65dc801f007ec395bda1da9b
ryurbsgks <ryurbsgks5114@gmail.com>
2022-11-14 23:31:36 +0900

    Feat: 상품 등록 toast ui 이미지 업로드 기능 추가
    * 상품 등록 toast ui 이미지 업로드 기능 추가

---
+++ server/controllers/product/editor.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+module.exports = (req, res) => {
+
+  let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "img/editor/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${file.originalname}`);
+    }
+  });
+  const upload = multer({ storage: storage }).single("img");
+
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(500).send({ message: "업로드 오류" });
+    }
+    // res.req.file.filename
+    res.status(200).send({ data: res.req.file.path, message: "업로드 성공" });
+  });
+
+};
