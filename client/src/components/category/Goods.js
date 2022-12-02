29371a90c5c84092a976ada22e54987d4bada63b
ryurbsgks <ryurbsgks5114@gmail.com>
2022-12-02 23:31:22 +0900

    Feat: Goods 컴포넌트 추가
    * Goods 컴포넌트 추가

---
+++ client/src/components/category/Goods.js
@@ -0,0 +1,40 @@
+import "../../App.css";
+import "./category.css";
+import { useState, useLayoutEffect } from "react";
+
+function Goods({ img, name, price, salePrice, salePct }) {
+
+  const [KRW, setKRW] = useState({
+    price: price.toLocaleString("ko-KR"),
+    salePrice
+  });
+
+  useLayoutEffect( () => {
+
+    if (salePrice) {
+      setKRW({
+        ...KRW,
+        salePrice: salePrice.toLocaleString("ko-KR")
+      });
+    }
+
+  }, []);
+
+  return (
+    <article className="goods">
+      <img src={`${process.env.REACT_APP_URL}/${img}`} alt="img" />
+      <div className="goods__title">{name}</div>
+      {salePrice 
+      ? <div className="goods__price-info">
+          <span className="goods__price-info__salePct">{salePct}%</span>
+          <span className="goods__price-info__price"><b>{KRW.price}</b>원</span>
+          <span className="goods__price-info__origin-price">{KRW.salePrice}원</span>
+        </div>
+      : <div className="goods__price-info">
+          <span className="goods__price-info__price"><b>{KRW.price}</b>원</span>
+        </div>}
+    </article>
+  );
+}
+
+export default Goods;
