94cf7a28706ceb633af6aee66d15b9b44b496da6
ryurbsgks <ryurbsgks5114@gmail.com>
2022-12-22 20:56:04 +0900

    Update: 판매자 닉네임 데이터 추가
    * 판매자 닉네임 데이터 추가

--- server/controllers/product/detail.js
+++ server/controllers/product/detail.js
@@ -1,8 +1,9 @@
-const { product } = require("../../models");
+const { product, user } = require("../../models");
 
 module.exports = (req, res) => {
 
   const id = req.query.id;
+  let data;
 
   product.findOne({
     where: {
@@ -13,7 +14,23 @@ module.exports = (req, res) => {
       return res.status(404).send({ message: "요청한 데이터가 존재하지 않습니다" });
     }
 
-    return res.status(200).send({data: result.dataValues, message: "상세페이지 요청 성공" });
+    data = result.dataValues;
+
+    user.findOne({
+      attributes: ["nickname"],
+      where: {id: result.dataValues.userID}
+    }).then( (result) => {
+      if (!result) {
+        return res.status(404).send({ message: "해당 게시물의 판매자가 존재하지 않습니다" });
+      }
+
+      data.nickname = result.dataValues.nickname;
+
+      return res.status(200).send({data: data, message: "상세페이지 요청 성공" });
+    }).catch( (err) => {
+      res.status(500).send({ message: err });
+    });
+
   }).catch( (err) => {
     res.status(500).send({ message: err });
   });
