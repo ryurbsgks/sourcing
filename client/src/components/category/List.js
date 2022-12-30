347f22ec84fad9669e2ebf7fe145ede174b810aa
ryurbsgks <ryurbsgks5114@gmail.com>
2022-12-30 19:17:51 +0900

    Update: List 페이지 업데이트
    * List 페이지 업데이트
    - url 쿼리 스트링에 따른 상품 업데이트

--- client/src/components/category/List.js
+++ client/src/components/category/List.js
@@ -1,13 +1,14 @@
 import "../../App.css";
 import "./category.css";
 import axios from "axios";
-import { useState, useLayoutEffect } from "react";
+import { useState, useLayoutEffect, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesLeft, faAngleLeft, faAngleRight, faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 import Goods from "./Goods";
 import Loading from "../common/Loading";
 
-function List({ params, sort }) {
+function List({ params, sort, page }) {
 
   const [data, setData] = useState();
   const GOOD_PER_PAGE = 16;
@@ -16,57 +17,57 @@ function List({ params, sort }) {
     totalGood: 0,
     totalPage: 0
   });
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState();
   const [displayPageGroup, setDisplayPageGroup] = useState(Math.ceil(currentPage / PAGE_PER_DISPLAY));
   const [pageGroup, setPageGroup] = useState({
     first: (displayPageGroup - 1) * PAGE_PER_DISPLAY + 1,
     last: displayPageGroup * PAGE_PER_DISPLAY
   });
 
-  useLayoutEffect( () => {
+  const navigate = useNavigate();
 
-    axios.get(`${process.env.REACT_APP_URL}/product/list`, {
-      params: {
-        category: params,
-        sortTarget: sort.target,
-        sortOption: sort.option,
-        currentPage: currentPage
-      }
-    }).then( (res) => {
-      if (res.data.message === "상품 리스트 조회 성공") {
-        setPagination({
-          ...pagination,
-          totalGood: res.data.count,
-          totalPage: Array.from({length: Math.ceil(res.data.count / GOOD_PER_PAGE)}, (x, i) => i + 1)
+  useEffect( () => {
+
+    if (isNaN(page) || page === "" || page === "0") {
+      return navigate("/404-page-not-found");
+    }
+
+    if (page === null) {
+      return setCurrentPage(1);
+    }
+
+    setCurrentPage(Number(page))
+
+  }, [page]);
+
+  useEffect( () => {
+
+    if (!isNaN(currentPage)) {
+
+      setDisplayPageGroup(Math.ceil(currentPage / PAGE_PER_DISPLAY));
+
+      if (sort === "best" || sort === "new" || sort === "lowprice" || sort === "heighprice" || sort === null) {
+
+        axios.get(`${process.env.REACT_APP_URL}/product/list`, {
+          params: {
+            category: params,
+            sort: sort,
+            currentPage: currentPage
+          }
+        }).then( (res) => {
+          if (res.data.message === "상품 리스트 조회 성공") {
+            setPagination({
+              totalGood: res.data.count,
+              totalPage: Array.from({length: Math.ceil(res.data.count / GOOD_PER_PAGE)}, (x, i) => i + 1)
+            });
+            return setData(res.data.data);
+          }
         });
-        return setData(res.data.data);
+
       }
-    });
-    
-  }, [params, sort]);
+    }
 
-  useLayoutEffect( () => {
-    setCurrentPage(1);
-  }, [params]);
-
-  useLayoutEffect( () => {
-
-    setDisplayPageGroup(Math.ceil(currentPage / PAGE_PER_DISPLAY));
-
-    axios.get(`${process.env.REACT_APP_URL}/product/list`, {
-      params: {
-        category: params,
-        sortTarget: sort.target,
-        sortOption: sort.option,
-        currentPage: currentPage
-      }
-    }).then( (res) => {
-      if (res.data.message === "상품 리스트 조회 성공") {
-        return setData(res.data.data);
-      }
-    });
-
-  }, [currentPage]);
+  }, [currentPage, params, sort]);
 
   useLayoutEffect( () => {
     setPageGroup({
@@ -76,20 +77,24 @@ function List({ params, sort }) {
   }, [displayPageGroup]);
 
   const handleClickPageNum = (e) => {
-    return setCurrentPage(e.target.value);
+    setCurrentPage(e.target.value);
+    sort ? navigate(`/category/${params}?sort=${sort}&page=${e.target.value}`) : navigate(`/category/${params}?page=${e.target.value}`);
   };
 
   const handleClickIcon = (e) => {
 
     if (e.currentTarget.value === -1) {
-      return setCurrentPage(currentPage - 1);
+      setCurrentPage(currentPage - 1);
+      return sort ? navigate(`/category/${params}?sort=${sort}&page=${currentPage - 1}`) : navigate(`/category/${params}?page=${currentPage - 1}`);
     }
 
     if (e.currentTarget.value === 2) {
-      return setCurrentPage(currentPage + 1);
+      setCurrentPage(currentPage + 1);
+      return sort ? navigate(`/category/${params}?sort=${sort}&page=${currentPage + 1}`) : navigate(`/category/${params}?page=${currentPage + 1}`);
     }
 
-    return setCurrentPage(e.currentTarget.value);
+    setCurrentPage(e.currentTarget.value);
+    return sort ? navigate(`/category/${params}?sort=${sort}&page=${e.currentTarget.value}`) : navigate(`/category/${params}?page=${e.currentTarget.value}`);
   }
 
   return (
