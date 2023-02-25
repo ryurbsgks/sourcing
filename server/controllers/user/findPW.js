e69c0a2ca4bef1eb1e8de2f037e649afb2d7ddf5
ryurbsgks <ryurbsgks5114@gmail.com>
2023-02-25 16:31:18 +0900

    Update: 비밀번호 찾기 업데이트
    * 비밀번호 찾기 업데이트
    - 비밀번호 찾기 시 임시 비밀번호 발급

--- server/controllers/user/findPW.js
+++ server/controllers/user/findPW.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer");
 const { send } = require("../../SMS");
 const { user } = require("../../models");
+const crypto = require("crypto");
 
 module.exports = (req, res) => {
 
   const userID = req.body.userID;
+  const newPassword = Math.random().toString(36).slice(2);
 
   if (req.body.tel) {
 
@@ -20,7 +22,19 @@ module.exports = (req, res) => {
         return res.status(404).send({ message: "입력하신 회원 정보가 맞는지 확인해주세요" });
       }
 
-      const content = `[오늘의 장] 비밀번호는 "${result.dataValues.pw}" 입니다`;
+      const password = crypto.pbkdf2Sync(newPassword, result.dataValues.salt, 9877, 64, "sha512").toString("base64");
+
+      user.update({
+        pw: password
+      }, {
+        where: {
+          userID: userID
+        }
+      }).catch( (err) => {
+        res.status(500).send({ message: err });
+      });
+
+      const content = `[오늘의 장] 임시 비밀번호는 "${newPassword}" 입니다`;
 
       send(tel, content);
       return res.status(200).send({ message: "핸드폰으로 비밀번호를 전송하였습니다" });
@@ -53,11 +67,23 @@ module.exports = (req, res) => {
         return res.status(404).send({ message: "입력하신 회원 정보가 맞는지 확인해주세요" });
       }
 
+      const password = crypto.pbkdf2Sync(newPassword, result.dataValues.salt, 9877, 64, "sha512").toString("base64");
+
+      user.update({
+        pw: password
+      }, {
+        where: {
+          userID: userID
+        }
+      }).catch( (err) => {
+        res.status(500).send({ message: err });
+      });
+
       const message = {
         from: process.env.NODEMAILER_USER,
         to: email,
-        subject: `[오늘의 장] 비밀번호 찾기 비밀번호를 알려드립니다`,
-        text: `[오늘의 장] 비밀번호는 "${result.dataValues.pw}" 입니다`
+        subject: `[오늘의 장] 비밀번호 찾기 임시 비밀번호를 알려드립니다`,
+        text: `[오늘의 장] 임시 비밀번호는 "${newPassword}" 입니다`
       };
 
       transporter.sendMail(message);
