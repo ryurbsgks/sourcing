5b462112a7eb3c4f032605522314fb0c5480f362
ryurbsgks <ryurbsgks5114@gmail.com>
2023-03-14 23:33:12 +0900

    Update: 장바구니 추가 기능 업데이트
    * 장바구니 추가 기능 업데이트
    - 장바구니 추가 시 count 컬럼 추가

--- server/controllers/product/insertCart.js
+++ server/controllers/product/insertCart.js
@@ -2,12 +2,12 @@ const { productcart } = require("../../models");
 
 module.exports = (req, res) => {
   
-  const userID = req.body.userID;
-  const productID = req.body.productID;
+  const { userID, productID, count } = req.body;
 
   productcart.create({
     userID: userID,
-    productID: productID
+    productID: productID,
+    count: count
   }).then( (result) => {
     if (!result) {
       return res.status(400).send({ message: "장바구니 추가 실패" });
