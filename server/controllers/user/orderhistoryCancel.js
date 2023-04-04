7a11e54ecbc7f08ddb5525593cffc444b580c939
ryurbsgks <ryurbsgks5114@gmail.com>
2023-04-04 16:46:09 +0900

    Update: Iamport 결제 취소 기능 추가
    * Iamport 결제 취소 기능 추가

--- server/controllers/user/orderhistoryCancel.js
+++ server/controllers/user/orderhistoryCancel.js
@@ -1,25 +1,71 @@
 const { orderHistory, saleHistory } = require("../../models");
+const axios = require("axios");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
 
-  const { merchant_uid, status } = req.body;
+  const { merchant_uid, status, cancel_request_amount, reason } = req.body;
+  const getAccessToken = await axios({
+    url: "https://api.iamport.kr/users/getToken",
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    data: {
+      imp_key: process.env.IAMPORT_REST_API_KEY,
+      imp_secret: process.env.IAMPORT_REST_API_SECRET
+    }
+  });
+  const { access_token } = getAccessToken.data.response;
 
-  orderHistory.update({
-    status: status
-  }, {
+  orderHistory.findOne({
     where: {
       merchant_uid: merchant_uid
-    }
-  }).then( () => {
+    },
+    raw: true
+  }).then( async (result) => {
 
-    saleHistory.update({
-      status: status
+    const paymentData = result;
+    const { imp_uid, amount, cancel_amount } = paymentData;
+    const cancelableAmount = amount - cancel_amount;
+
+    if (cancelableAmount <= 0) {
+      return res.status(400).send({ message: "이미 전액환불된 주문입니다." });
+    }
+
+    await axios({
+      url: "https://api.iamport.kr/payments/cancel",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${access_token}`
+      },
+      data: {
+        reason: reason,
+        imp_uid: imp_uid,
+        amount: cancel_request_amount,
+        checksum: cancelableAmount
+      }
+    });
+
+    orderHistory.update({
+      status: status,
+      cancel_amount: cancel_request_amount
     }, {
       where: {
         merchant_uid: merchant_uid
       }
     }).then( () => {
-      return res.status(201).send({ message: "구매 취소 성공" });
+
+      saleHistory.update({
+        status: status
+      }, {
+        where: {
+          merchant_uid: merchant_uid
+        }
+      }).then( () => {
+        return res.status(201).send({ message: "구매 취소 성공" });
+      }).catch( (err) => {
+        return res.status(500).send({ message: err });
+      });
+
     }).catch( (err) => {
       return res.status(500).send({ message: err });
     });
